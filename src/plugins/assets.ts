3879c83c28b87899f33048b668a5e6f6d988c236
zhoubo416 <409970551@qq.com>
2023-12-20 11:48:47 +0800

    增加样式

--- src/plugins/assets.ts
+++ src/plugins/assets.ts
@@ -6,6 +6,8 @@ import 'virtual:svg-icons-register';
 import '../assets/styles/css/global.css';
 import '@/utils/rem.ts';
 
+import '@unocss/reset/normalize.css';
+
 /** import static assets: css, js , font and so on. - [引入静态资源，css、js和字体文件等] */
 export default function setupAssets() {
   //
