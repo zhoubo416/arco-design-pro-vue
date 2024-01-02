6610ba4dbb155f22166aead81a0d135bc55272e7
zhoubo416 <409970551@qq.com>
2024-01-02 17:25:29 +0800

    调试表格

---
+++ src/views/component/table/form.vue
@@ -0,0 +1,207 @@
+<template>
+  <a-form
+    ref="formRef"
+    size="mini"
+    :model="form"
+    class="w-full"
+    style="height: 580px"
+    layout="vertical"
+    @submit="handleSubmit"
+  >
+    <a-form-item>
+      <a-space>
+        <a-button html-type="submit">查询</a-button>
+        <a-button @click="$refs.formRef.resetFields()">重置</a-button>
+      </a-space>
+    </a-form-item>
+    <a-form-item
+      field="name"
+      label="名称"
+      :rules="[
+        { required: true, message: 'name is required' },
+        { minLength: 5, message: 'must be greater than 5 characters' },
+      ]"
+      :validate-trigger="['change', 'input']"
+    >
+      <a-input v-model="form.name" placeholder="please enter your username..." />
+    </a-form-item>
+    <a-form-item
+      field="age"
+      label="年龄"
+      :rules="[
+        { required: true, message: 'age is required' },
+        { type: 'number', max: 200, message: 'age is max than 200' },
+      ]"
+    >
+      <a-input-number v-model="form.age" placeholder="please enter your age..." />
+    </a-form-item>
+    <a-form-item
+      field="section"
+      label="区域"
+      :rules="[{ match: /section one/, message: 'must select one' }]"
+    >
+      <a-select v-model="form.section" placeholder="Please select ..." allow-clear>
+        <a-option value="section one">Section One</a-option>
+        <a-option value="section two">Section Two</a-option>
+        <a-option value="section three">Section Three</a-option>
+      </a-select>
+    </a-form-item>
+    <a-form-item
+      field="province"
+      label="省市"
+      :rules="[{ required: true, message: 'province is required' }]"
+    >
+      <a-cascader
+        v-model="form.province"
+        :options="options"
+        placeholder="Please select ..."
+        allow-clear
+      />
+    </a-form-item>
+    <a-form-item
+      field="options"
+      label="选项"
+      :rules="[{ type: 'array', minLength: 2, message: 'must select greater than two options' }]"
+    >
+      <a-checkbox-group v-model="form.options">
+        <a-checkbox value="option one">Section One</a-checkbox>
+        <a-checkbox value="option two">Option Two</a-checkbox>
+        <a-checkbox value="option three">Option Three</a-checkbox>
+        <a-checkbox value="option four">Option Four</a-checkbox>
+      </a-checkbox-group>
+    </a-form-item>
+    <a-form-item field="date" label="日期">
+      <a-date-picker v-model="form.date" placeholder="Please select ..." />
+    </a-form-item>
+    <a-form-item field="time" label="时间">
+      <a-time-picker v-model="form.time" placeholder="Please select ..." />
+    </a-form-item>
+    <a-form-item field="radio" label="单选" :rules="[{ match: /one/, message: 'must select one' }]">
+      <a-radio-group v-model="form.radio">
+        <a-radio value="radio one">Radio One</a-radio>
+        <a-radio value="radio two">Radio Two</a-radio>
+      </a-radio-group>
+    </a-form-item>
+    <a-form-item
+      field="slider"
+      label="滑块"
+      :rules="[{ type: 'number', min: 5, message: 'slider is min than 5' }]"
+    >
+      <a-slider v-model="form.slider" :max="10" />
+    </a-form-item>
+    <a-form-item field="score" label="Score">
+      <a-rate v-model="form.score" allow-clear />
+    </a-form-item>
+    <a-form-item
+      field="switch"
+      label="切换"
+      :rules="[{ type: 'boolean', true: true, message: 'must be true' }]"
+    >
+      <a-switch v-model="form.switch" />
+    </a-form-item>
+    <a-form-item field="multiSelect" label="多选">
+      <a-select v-model="form.multiSelect" placeholder="Please select ..." multiple>
+        <a-option value="section one">Section One</a-option>
+        <a-option value="section two">Section Two</a-option>
+        <a-option value="section three">Section Three</a-option>
+      </a-select>
+    </a-form-item>
+    <a-form-item field="treeSelect" label="树型多选">
+      <a-tree-select :data="treeData" v-model="form.treeSelect" placeholder="Please select ..." />
+    </a-form-item>
+  </a-form>
+</template>
+
+<script setup>
+  import { reactive } from 'vue';
+  defineOptions({ name: 'TableForm' });
+
+  const form = reactive({
+    size: 'medium',
+    name: '',
+    age: undefined,
+    section: '',
+    province: 'haidian',
+    options: [],
+    date: '',
+    time: '',
+    radio: 'radio one',
+    slider: 5,
+    score: 5,
+    switch: false,
+    multiSelect: ['section one'],
+    treeSelect: '',
+  });
+  const options = [
+    {
+      value: 'beijing',
+      label: 'Beijing',
+      children: [
+        {
+          value: 'chaoyang',
+          label: 'ChaoYang',
+          children: [
+            {
+              value: 'datunli',
+              label: 'Datunli',
+            },
+          ],
+        },
+        {
+          value: 'haidian',
+          label: 'Haidian',
+        },
+        {
+          value: 'dongcheng',
+          label: 'Dongcheng',
+        },
+        {
+          value: 'xicheng',
+          label: 'XiCheng',
+        },
+      ],
+    },
+    {
+      value: 'shanghai',
+      label: 'Shanghai',
+      children: [
+        {
+          value: 'shanghaishi',
+          label: 'Shanghai',
+          children: [
+            {
+              value: 'huangpu',
+              label: 'Huangpu',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  const treeData = [
+    {
+      key: 'node1',
+      title: 'Node1',
+      children: [
+        {
+          key: 'node2',
+          title: 'Node2',
+        },
+      ],
+    },
+    {
+      key: 'node3',
+      title: 'Node3',
+      children: [
+        {
+          key: 'node4',
+          title: 'Node4',
+        },
+        {
+          key: 'node5',
+          title: 'Node5',
+        },
+      ],
+    },
+  ];
+</script>
--- src/views/component/table/index.vue
+++ src/views/component/table/index.vue
@@ -1,9 +1,145 @@
 <template>
-  <div>
-    <h1>Table</h1>
-  </div>
+  <a-card title="耗材查询" :bordered="false" size="small" class="shadow-sm rounded-16px w-full">
+    <a-layout>
+      <a-layout-sider :resize-directions="['right']">
+        <a-card :bordered="false" size="small" class="shadow-sm rounded-16px w-full">
+          <table-form />
+        </a-card>
+      </a-layout-sider>
+      <a-layout-content>
+        <div ref="listTableRef" style="width: 100%; height: 100%"></div>
+      </a-layout-content>
+    </a-layout>
+  </a-card>
 </template>
 
-<script lang="ts" setup></script>
+<script setup>
+  import { onMounted, ref, reactive } from 'vue';
+  import tableForm from './form.vue';
 
-<style lang="less" scoped></style>
+  import * as VTable from '@visactor/vtable';
+  const ListTable = VTable.ListTable;
+  import { DateInputEditor, InputEditor, ListEditor } from '@visactor/vtable-editors';
+  const inputEditor = new InputEditor();
+  const dateInputEditor = new DateInputEditor();
+  const listEditor = new ListEditor({ values: ['女', '男'] });
+
+  VTable.register.editor('name-editor', inputEditor);
+  VTable.register.editor('date-editor', dateInputEditor);
+  VTable.register.editor('list-editor', listEditor);
+
+  const listTableRef = ref();
+
+  const record = {
+    230517143221027: 'CA-2018-156720',
+    230517143221030: 'JM-15580',
+    230517143221032: 'Bagged Rubber Bands',
+    230517143221023: 'Office Supplies',
+    230517143221034: 'Fasteners',
+    230517143221037: 'West',
+    230517143221024: 'Loveland',
+    230517143221029: '2018-12-30',
+    230517143221042: '3',
+    230517143221040: '3.024',
+    230517143221041: '-0.605',
+  };
+  const records = Array(100).fill({ ...record });
+
+  const columns = [
+    {
+      field: '230517143221027',
+      title: 'Order ID',
+      width: 'auto',
+    },
+    {
+      field: '230517143221030',
+      title: 'Customer ID',
+      width: 'auto',
+    },
+    {
+      field: '230517143221032',
+      title: 'Product Name',
+      width: 'auto',
+      editor: (args) => {
+        if (args.row % 2 === 0) return 'list-editor';
+        else return 'name-editor';
+      },
+      cellType: 'text',
+      sort: true, // 使用内置默认排序逻辑
+    },
+    {
+      field: '230517143221023',
+      title: 'Category',
+      width: 'auto',
+      style: {
+        textAlign: 'left',
+        textBaseline: 'middle',
+        textOverflow: 'ellipsis',
+        color: '#F66',
+        fontSize: 14,
+        fontFamily: 'Arial',
+        fontWeight: 'bold',
+        fontVariant: 'small-caps',
+        fontStyle: 'italic',
+      },
+    },
+    {
+      field: '230517143221034',
+      title: 'Sub-Category',
+      width: 'auto',
+    },
+    {
+      field: '230517143221037',
+      title: 'Region',
+      width: 'auto',
+    },
+    {
+      field: '230517143221024',
+      title: 'City',
+      width: 'auto',
+    },
+    {
+      field: '230517143221029',
+      title: 'Order Date',
+      width: 'auto',
+    },
+    {
+      field: '230517143221042',
+      title: 'Quantity',
+      width: 'auto',
+    },
+    {
+      field: '230517143221040',
+      title: 'Sales',
+      width: 'auto',
+    },
+    {
+      field: '230517143221041',
+      title: 'Profit',
+      width: 'auto',
+    },
+  ];
+
+  const option = {
+    records,
+    columns,
+    widthMode: 'standard',
+  };
+
+  onMounted(() => {
+    const listTable = new ListTable(listTableRef.value, option);
+
+    listTable.on('click_cell', (params) => {
+      console.log(params, 'click_cell');
+    });
+    listTable.on('change_cell_value', (params) => {
+      console.log(params, 'changeCellValue');
+    });
+    listTable.on('start_edit_cell', (params) => {
+      console.log(params, 'startEditCell');
+    });
+    listTable.on('complete_edit_cell', (params) => {
+      console.log(params, 'completeEditCell');
+    });
+  });
+</script>
