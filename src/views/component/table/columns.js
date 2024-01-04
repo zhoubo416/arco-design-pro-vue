8c5707ee79205ddbe2afaa74d3f8f6b6c76f6741
zhoubo416 <409970551@qq.com>
2024-01-04 17:24:35 +0800

    增加自定义

--- src/views/component/table/columns.js
+++ src/views/component/table/columns.js
@@ -1,310 +1,395 @@
 import * as VTable from '@visactor/vtable';
 
+const customEditIcon = {
+  type: 'svg', //指定svg格式图标，其他还支持path，image
+  svg: `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32"><path d="M2 26h28v2H2z" fill="currentColor"></path><path d="M25.4 9c.8-.8.8-2 0-2.8l-3.6-3.6c-.8-.8-2-.8-2.8 0l-15 15V24h6.4l15-15zm-5-5L24 7.6l-3 3L17.4 7l3-3zM6 22v-3.6l10-10l3.6 3.6l-10 10H6z" fill="currentColor"></path></svg>`,
+  width: 16,
+  height: 16,
+  name: 'edit', //定义图标的名称，在内部会作为缓存的key值
+  positionType: VTable.TYPES.IconPosition.contentRight, // 指定位置，可以在文本的前后，或者在绝对定位在单元格的左侧右侧
+  visibleTime: 'always', // 显示时机， 'always' | 'mouseenter_cell' | 'click_cell'
+  hover: {
+    // 热区大小
+    width: 12,
+    height: 12,
+    bgColor: 'rgba(22,44,66,0.5)',
+  },
+  tooltip: {
+    style: { arrowMark: false },
+    // 气泡框，按钮的的解释信息
+    title: '编辑',
+    placement: VTable.TYPES.Placement.right,
+  },
+};
+
 export const columns = [
   {
-    "field": "idx",
-    "title": "序号",
-    "width": "auto",
-     style: {
-      textAlign: 'center'
-     }
-},
+    field: 'idx',
+    title: '序号',
+    width: 'auto',
+    style: {
+      textAlign: 'center',
+    },
+  },
   {
-      "field": "hospitalName",
-      "title": "医院名称",
-      "width": "auto",cellType: 'link',
-      templateLink: 'https://www.google.com.hk/search?q={hospitalName}',
-      style: {
-       textAlign: 'center'
+    headerType: 'checkbox', //指定表头单元格显示为复选框
+    cellType: 'checkbox', //指定body单元格显示为复选框
+    field: 'isCheck',
+    width: '50',
+    style: {
+      textAlign: 'center',
+    },
+  },
+  {
+    field: 'hospitalName',
+    title: '医院名称',
+    width: 'auto',
+    cellType: 'link',
+    templateLink: 'https://www.google.com.hk/search?q={hospitalName}',
+    style: {
+      textAlign: 'center',
+    },
+    sort: true,
+    sort: (v1, v2, order) => {
+      // console.log(v1, v2, order, 'v1, v2, order');
+      // 使用自定义排序逻辑
+      if (order === 'desc') {
+        return v1 === v2 ? 0 : v1 > v2 ? -1 : 1;
+      }
+      return v1 === v2 ? 0 : v1 > v2 ? 1 : -1;
+    },
+  },
+  {
+    field: 'organizationName',
+    title: '供应商',
+    width: '200',
+    sort: true,
+    style: {
+      textAlign: 'left',
+      textBaseline: 'middle',
+      textOverflow: 'ellipsis',
+      color: '#F66',
+      fontSize: 14,
+      fontFamily: 'Arial',
+      fontWeight: 'bold',
+      fontVariant: 'small-caps',
+      fontStyle: 'italic',
+    },
+    editor: 'name-editor',
+    sort: (v1, v2, order) => {
+      // console.log(v1, v2, order, 'v1, v2, order');
+      // 使用自定义排序逻辑
+      if (order === 'desc') {
+        return v1 === v2 ? 0 : v1 > v2 ? -1 : 1;
+      }
+      return v1 === v2 ? 0 : v1 > v2 ? 1 : -1;
+    },
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'provinceCode1',
+    title: '省平台编码1',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'provinceCode2',
+    title: '省平台编码2',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'provinceCode3',
+    title: '省平台编码3',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+    fieldFormat: (record) => '字典转换省3',
+  },
+  {
+    field: 'materialUseTypeName',
+    title: '物资类型',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'materialCode',
+    title: '耗材编码',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'registerCode',
+    title: '注册证号',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'hospitalCommodityCode',
+    title: '医院物资编码',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'materialName',
+    title: '耗材名称',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'materialAlias',
+    title: '耗材别名',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'materialSpecModel',
+    title: '规格型号',
+    width: 'auto',
+    editor: 'name-editor',
+    headerIcon: customEditIcon,
+  },
+  {
+    field: 'materialUnit',
+    title: '单位',
+    width: 'auto',
+    style: {
+      textAlign: 'right',
+    },
+  },
+  {
+    field: 'materialPackage',
+    title: '包装规格',
+    width: 'auto',
+  },
+  {
+    field: 'materialProductCode',
+    title: '产品编码',
+    width: 'auto',
+  },
+  {
+    field: 'materialProductName',
+    title: '产品名称',
+    width: 'auto',
+  },
+  {
+    field: 'materialBrandName',
+    title: '品牌',
+    width: 'auto',
+  },
+  {
+    field: 'manufactureName',
+    title: '生产厂家',
+    width: 'auto',
+  },
+  {
+    field: 'chargePrice',
+    title: 'his收费价格（元）',
+    width: 'auto',
+    style: {
+      textAlign: 'right',
+    },
+  },
+  {
+    field: 'materialPrice',
+    title: '在用单价（元）',
+    width: 'auto',
+    style: {
+      textAlign: 'right',
+    },
+  },
+  {
+    field: 'newMaterialPrice',
+    title: '新单价（元）',
+    width: 'auto',
+    style: {
+      textAlign: 'right',
+    },
+  },
+  {
+    field: 'newPriceStartTime',
+    title: '新单价执行时间',
+    width: 'auto',
+  },
+  {
+    field: 'priceBase',
+    title: '价格依据',
+    width: 'auto',
+  },
+  {
+    field: 'saleYn',
+    title: '是否寄售',
+    width: 'auto',
+  },
+  {
+    field: 'authorizeStartTime',
+    title: '授权有效期起',
+    width: 'auto',
+  },
+  {
+    field: 'authorizeEndTime',
+    title: '授权有效期止',
+    width: 'auto',
+  },
+  {
+    field: 'accountPeriod',
+    title: '在用付款账期（月）',
+    width: 'auto',
+  },
+  {
+    field: 'newAccountPeriod',
+    title: '新付款账期',
+    width: 'auto',
+  },
+  {
+    field: 'newAccountPeriodStartTime',
+    title: '新付款账期执行时间',
+    width: 'auto',
+  },
+  {
+    field: 'accountYn',
+    title: '耗材是否可以结账',
+    width: 'auto',
+  },
+  {
+    field: 'materialManufactureType',
+    title: '耗材类别',
+    width: 'auto',
+  },
+  {
+    field: 'materialChoiceType',
+    title: '耗材类型',
+    width: 'auto',
+  },
+  {
+    field: 'shippingManagementName',
+    title: '发运管理',
+    width: '160',
+  },
+  {
+    field: 'storeCondition',
+    title: '存储条件',
+    width: 'auto',
+  },
+  {
+    field: 'purchaseStyle',
+    title: '采购方式',
+    width: 'auto',
+  },
+  {
+    field: 'volumeBasedPurchaseYnName',
+    title: '是否带量采购',
+    width: 'auto',
+  },
+  {
+    field: 'tendersStyle',
+    title: '招标方式',
+    width: 'auto',
+  },
+  {
+    field: 'contractNo',
+    title: '合同编号',
+    width: 'auto',
+  },
+  {
+    field: 'position',
+    title: '合同存放位置',
+    width: 'auto',
+  },
+  {
+    field: 'materialStartTime',
+    title: '启用时间',
+    width: 'auto',
+  },
+  {
+    field: 'materialStopTime',
+    title: '停用时间',
+    width: 'auto',
+  },
+  {
+    field: 'fromMaterialStatus',
+    title: '耗材状态',
+    width: 'auto',
+  },
+  {
+    field: 'materialStatus',
+    title: '清单状态',
+    width: 'auto',
+  },
+  {
+    field: 'expiredYn',
+    title: '是否已过期',
+    width: 'auto',
+  },
+  {
+    field: 'updateUserName',
+    title: '修改人',
+    width: 'auto',
+  },
+  {
+    field: 'updateTime2',
+    title: '修改时间',
+    width: 'auto',
+  },
+  {
+    field: 'remark',
+    title: '备注',
+    width: 'auto',
+  },
+  {
+    field: 'auditRemark',
+    title: '准入审核备注',
+    width: 'auto',
+  },
+  {
+    field: 'action',
+    title: '操作',
+    width: '120',
+    style: {
+      bgColor: 'rgb(242,242,242)',
+    },
+
+    icon: [
+      {
+        name: 'edit',
+        type: 'svg',
+        marginLeft: 10,
+        color: 'blue',
+        positionType: VTable.TYPES.IconPosition.left,
+        width: 20,
+        height: 20,
+        svg: 'https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/edit.svg',
+        tooltip: {
+          style: { arrowMark: true },
+          // 气泡框，按钮的的解释信息
+          title: '编辑',
+          placement: VTable.TYPES.Placement.right,
+        },
       },
-      sort: true,
+      {
+        name: 'delete',
+        type: 'svg',
+        marginLeft: 20,
+        color: 'blue',
+        positionType: VTable.TYPES.IconPosition.left,
+        width: 20,
+        height: 20,
+        svg: 'https://lf9-dp-fe-cms-tos.byteorg.com/obj/bit-cloud/VTable/delete.svg',
+        tooltip: {
+          style: { arrowMark: true },
+          // 气泡框，按钮的的解释信息
+          title: '删除',
+          placement: VTable.TYPES.Placement.right,
+        },
+      },
+    ],
   },
-  {
-      "field": "organizationName",
-      "title": "供应商",
-      "width": "200",
-      sort: true,
-      editor: 'name-editor',
-      customLayout(args){
-        const { width, height}= args.rect;
-        const {dataValue,table,row } =args;
-        const elements=[];
-        let top=30;
-        const left=15;
-
-        const container = new VTable.CustomLayout.Group({
-          height,
-          width,
-          display: 'flex',
-          flexDirection: 'row',
-          alignContent: 'center',
-          alignItems: 'center',
-          justifyContent: 'space-around',
-        });
-
-        const text = new VTable.CustomLayout.Text({
-          text: row===1? '-----供应商2-----':'-----4444--供应商---4444----',
-          maxLineWidth: 140,
-          pickable: true
-        });
-
-        container.add(text);
-        
-        return {
-          rootContainer: container,
-        }
-      }
-  },
-  {
-      "field": "provinceCode1",
-      "title": "省平台编码1",
-      "width": "auto",
-      editor: 'name-editor'
-  },
-  {
-      "field": "provinceCode2",
-      "title": "省平台编码2",
-      "width": "auto",
-      editor: 'name-editor'
-  },
-  {
-      "field": "provinceCode3",
-      "title": "省平台编码3",
-      "width": "auto"
-  },
-  {
-      "field": "materialUseTypeName",
-      "title": "物资类型",
-      "width": "auto"
-  },
-  {
-      "field": "materialCode",
-      "title": "耗材编码",
-      "width": "auto"
-  },
-  {
-      "field": "registerCode",
-      "title": "注册证号",
-      "width": "auto"
-  },
-  {
-      "field": "hospitalCommodityCode",
-      "title": "医院物资编码",
-      "width": "auto"
-  },
-  {
-      "field": "materialName",
-      "title": "耗材名称",
-      "width": "auto"
-  },
-  {
-      "field": "materialAlias",
-      "title": "耗材别名",
-      "width": "auto"
-  },
-  {
-      "field": "materialSpecModel",
-      "title": "规格型号",
-      "width": "auto"
-  },
-  {
-      "field": "materialUnit",
-      "title": "单位",
-      "width": "auto",
-      style: {
-       textAlign: 'right'
-      }
-  },
-  {
-      "field": "materialPackage",
-      "title": "包装规格",
-      "width": "auto"
-  },
-  {
-      "field": "materialProductCode",
-      "title": "产品编码",
-      "width": "auto"
-  },
-  {
-      "field": "materialProductName",
-      "title": "产品名称",
-      "width": "auto"
-  },
-  {
-      "field": "materialBrandName",
-      "title": "品牌",
-      "width": "auto"
-  },
-  {
-      "field": "manufactureName",
-      "title": "生产厂家",
-      "width": "auto"
-  },
-  {
-      "field": "chargePrice",
-      "title": "his收费价格（元）",
-      "width": "auto",
-      style: {
-       textAlign: 'right'
-      }
-  },
-  {
-      "field": "materialPrice",
-      "title": "在用单价（元）",
-      "width": "auto",
-      style: {
-       textAlign: 'right'
-      }
-  },
-  {
-      "field": "newMaterialPrice",
-      "title": "新单价（元）",
-      "width": "auto",
-      style: {
-       textAlign: 'right'
-      }
-  },
-  {
-      "field": "newPriceStartTime",
-      "title": "新单价执行时间",
-      "width": "auto"
-  },
-  {
-      "field": "priceBase",
-      "title": "价格依据",
-      "width": "auto"
-  },
-  {
-      "field": "saleYn",
-      "title": "是否寄售",
-      "width": "auto"
-  },
-  {
-      "field": "authorizeStartTime",
-      "title": "授权有效期起",
-      "width": "auto"
-  },
-  {
-      "field": "authorizeEndTime",
-      "title": "授权有效期止",
-      "width": "auto"
-  },
-  {
-      "field": "accountPeriod",
-      "title": "在用付款账期（月）",
-      "width": "auto"
-  },
-  {
-      "field": "newAccountPeriod",
-      "title": "新付款账期",
-      "width": "auto"
-  },
-  {
-      "field": "newAccountPeriodStartTime",
-      "title": "新付款账期执行时间",
-      "width": "auto"
-  },
-  {
-      "field": "accountYn",
-      "title": "耗材是否可以结账",
-      "width": "auto"
-  },
-  {
-      "field": "materialManufactureType",
-      "title": "耗材类别",
-      "width": "auto"
-  },
-  {
-      "field": "materialChoiceType",
-      "title": "耗材类型",
-      "width": "auto"
-  },
-  {
-      "field": "shippingManagementName",
-      "title": "发运管理",
-      "width": "auto"
-  },
-  {
-      "field": "storeCondition",
-      "title": "存储条件",
-      "width": "auto"
-  },
-  {
-      "field": "purchaseStyle",
-      "title": "采购方式",
-      "width": "auto"
-  },
-  {
-      "field": "volumeBasedPurchaseYnName",
-      "title": "是否带量采购",
-      "width": "auto"
-  },
-  {
-      "field": "tendersStyle",
-      "title": "招标方式",
-      "width": "auto"
-  },
-  {
-      "field": "contractNo",
-      "title": "合同编号",
-      "width": "auto"
-  },
-  {
-      "field": "position",
-      "title": "合同存放位置",
-      "width": "auto"
-  },
-  {
-      "field": "materialStartTime",
-      "title": "启用时间",
-      "width": "auto"
-  },
-  {
-      "field": "materialStopTime",
-      "title": "停用时间",
-      "width": "auto"
-  },
-  {
-      "field": "fromMaterialStatus",
-      "title": "耗材状态",
-      "width": "auto"
-  },
-  {
-      "field": "materialStatus",
-      "title": "清单状态",
-      "width": "auto"
-  },
-  {
-      "field": "expiredYn",
-      "title": "是否已过期",
-      "width": "auto"
-  },
-  {
-      "field": "updateUserName",
-      "title": "修改人",
-      "width": "auto"
-  },
-  {
-      "field": "updateTime2",
-      "title": "修改时间",
-      "width": "auto"
-  },
-  {
-      "field": "remark",
-      "title": "备注",
-      "width": "auto"
-  },
-  {
-      "field": "auditRemark",
-      "title": "准入审核备注",
-      "width": "auto"
-  },
-  {
-      "field": "action",
-      "title": "操作",
-      "width": "180",
-      style: {
-        bgColor: 'rgb(242,242,242)'
-       },
-  }
-]
+];
--- src/views/component/table/index.vue
+++ src/views/component/table/index.vue
@@ -37,11 +37,14 @@
 </template>
 
 <script setup>
-  import { onMounted, ref, watch } from 'vue';
+  import { onMounted, reactive, ref, watch } from 'vue';
+  import { Message } from '@arco-design/web-vue';
+
   import tableForm from './form.vue';
-  import {columns} from './columns'
-  import {tableData2} from './data';
-  const records = tableData2.data.rows.map((it,index) => ({...it,idx:index+1}));
+  import { columns } from './columns';
+  import { tableData2 } from './data';
+  const tableData = tableData2.data.rows.map((it, index) => ({ ...it, idx: index + 1,isCheck: false }));
+  const records = reactive(tableData) ;
 
   import * as VTable from '@visactor/vtable';
   const ListTable = VTable.ListTable;
@@ -62,10 +65,31 @@
     widthMode: 'standard',
     heightMode: 'standard',
     frozenColCount: 2,
-    allowFrozenColCount: 4,
+    allowFrozenColCount: 2,
     dragHeaderMode: 'all',
-    rightFrozenColCount:1
-
+    rightFrozenColCount: 1,
+    hover: {
+      highlightMode: 'cross',
+    },
+    editCellTrigger: 'click',
+    menu: {
+      renderMode: 'html',
+      defaultHeaderMenuItems: [
+        {
+          text: '筛选',
+          icon: {
+            svg: '<svg width="14" height="14" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M71 24L88 41.0286V53.0005L74.996 39.9755L74.9968 88.0005H66.9968L66.996 39.9835L54 53.0005V41.0286L71 24ZM48 80V88H8V80H48ZM48 44V52H8V44H48ZM88 8V16H8V8H88Z" fill="#2e2f32" fill-opacity="0.9"></path></svg>',
+          },
+          selectedIcon: {
+            svg: '<svg width="14" height="14" viewBox="0 0 96 96" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M71 24L88 41.0286V53.0005L74.996 39.9755L74.9968 88.0005H66.9968L66.996 39.9835L54 53.0005V41.0286L71 24ZM48 80V88H8V80H48ZM48 44V52H8V44H48ZM88 8V16H8V8H88Z" fill="rgb(55,145,255)" fill-opacity="0.9"></path></svg>',
+          },
+          stateIcon: {
+            svg: '<svg width="12" height="11" viewBox="0 0 64 64" fill="#161616" xmlns="http://www.w3.org/2000/svg" class="visactor-table-action-area-icon"><path fill-rule="evenodd" clip-rule="evenodd" d="M45.0144 25.4994L34.9864 15.4674L34.9864 60.0154H28.9864L28.9864 15.5334L18.9864 25.5294V17.0454L27.776 8.25802H27.7732L32.0158 4.01538L45.0144 17.0134V25.4994Z" fill="#161616" fill-opacity="0.9"></path></svg>',
+          },
+        },
+      ],
+      contextMenuItems: ['复制', '粘贴'],
+    },
   };
   let listTable = null;
 
@@ -76,6 +100,47 @@
     // listTable.on('change_cell_value', (params) => {});
     // listTable.on('start_edit_cell', (params) => {});
     // listTable.on('complete_edit_cell', (params) => {});
+    listTable.on('selected_cell', (params) => {
+      Message.info('selected_cell');
+    });
+    listTable.on('click_cell', (args) => {
+      const { col, row, targetIcon, originData } = args;
+      // console.log(args, 'args')
+      if (targetIcon) {
+        if (targetIcon.name === 'edit') {
+          Message.info('编辑第 ' + (originData.organizationName) + '的数据');
+          // window?.alert?.('编辑第 ' + (row - listTable.columnHeaderLevelCount + 1) + ' 条数据');
+        } else if (targetIcon.name === 'delete') {
+          records.splice(row - listTable.columnHeaderLevelCount, 1);
+          listTable.setRecords(records);
+          Message.error('删除第 ' + (originData.organizationName) + ' 条数据');
+        }
+      }
+    });
+    listTable.on('dropdown_icon_click', (params) => {
+      Message.info('dropdown_icon_click!');
+    });
+    listTable.on('dropdown_menu_click', (params) => {
+      Message.info('dropdown_menu_click!');
+    });
+    listTable.on('mouseenter_cell', (args) => {
+      const { col, row, targetIcon } = args;
+      if (col < 2 || row < 1) {
+        return;
+      }
+      const rect = listTable.getVisibleCellRangeRelativeRect({ col, row });
+      listTable.showTooltip(col, row, {
+        content: listTable.getCellValue(col, row),
+        referencePosition: { rect, placement: VTable.TYPES.Placement.right }, //TODO
+        className: 'defineTooltip',
+        style: {
+          bgColor: 'black',
+          color: 'white',
+          font: 'normal bold normal 14px/1 STKaiti',
+          arrowMark: true,
+        },
+      });
+    });
   }
 
   onMounted(() => {
