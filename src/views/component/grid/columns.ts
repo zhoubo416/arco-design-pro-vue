21b533a99db8af677e82eb3402ffc07e3f488842
zhoubo416 <409970551@qq.com>
2024-01-26 17:27:09 +0800

    调试 ag-grid

---
+++ src/views/component/grid/columns.ts
@@ -0,0 +1,243 @@
+// 定义所需的参数和返回类型
+interface ICellRendererParams {
+  node: {
+    rowIndex: number;
+  };
+}
+
+class RowIndexRenderer {
+  // 定义 eGui 为一个私有属性，并初始化为 any 类型，以便在 init 方法中赋值
+  private eGui: HTMLElement | null = null;
+
+  // 为 init 方法添加参数类型注解
+  init(params: ICellRendererParams) {
+    this.eGui = document.createElement('div');
+    this.eGui.textContent = '' + (params.node.rowIndex + 1);
+  }
+
+  // refresh 方法返回 boolean 类型，参数类型为 any，因为没有明确指定
+  refresh(params: any): boolean {
+    return false;
+  }
+
+  // getGui 方法返回 HTMLElement 类型
+  getGui(): HTMLElement | null {
+    return this.eGui;
+  }
+}
+
+export const columns = [
+  {
+    headerName: '#',
+    maxWidth: 100,
+    cellRenderer: RowIndexRenderer,
+    headerCheckboxSelection: true,
+    checkboxSelection: true,
+    showDisabledCheckboxes: true,
+    pinned: 'left',
+    editable: false,
+  },
+  {
+    field: 'hospitalName',
+    headerName: '医院名称',
+  },
+  {
+    field: 'organizationName',
+    headerName: '供应商',
+  },
+  {
+    field: 'provinceCode1',
+    headerName: '省平台编码1',
+  },
+  {
+    field: 'provinceCode2',
+    headerName: '省平台编码2',
+    valueParser: (params) => `${params.value}:没有值，用 parser 处理`,
+  },
+  {
+    field: 'provinceCode3',
+    headerName: '省平台编码3',
+  },
+  {
+    field: 'materialUseTypeName',
+    headerName: '物资类型',
+  },
+  {
+    field: 'materialCode',
+    headerName: '耗材编码',
+  },
+  {
+    field: 'registerCode',
+    headerName: '注册证号',
+  },
+  {
+    field: 'hospitalCommodityCode',
+    headerName: '医院物资编码',
+  },
+  {
+    field: 'materialName',
+    headerName: '耗材名称',
+  },
+  {
+    field: 'materialAlias',
+    headerName: '耗材别名',
+  },
+  {
+    field: 'materialSpecModel',
+    headerName: '规格型号',
+  },
+  {
+    field: 'materialUnit',
+    headerName: '单位',
+  },
+  {
+    field: 'materialPackage',
+    headerName: '包装规格',
+  },
+  {
+    field: 'materialProductCode',
+    headerName: '产品编码',
+  },
+  {
+    field: 'materialProductName',
+    headerName: '产品名称',
+  },
+  {
+    field: 'materialBrandName',
+    headerName: '品牌',
+  },
+  {
+    field: 'manufactureName',
+    headerName: '生产厂家',
+  },
+  {
+    field: 'chargePrice',
+    headerName: 'his收费价格（元）',
+  },
+  {
+    field: 'materialPrice',
+    headerName: '在用单价（元）',
+  },
+  {
+    field: 'newMaterialPrice',
+    headerName: '新单价（元）',
+  },
+  {
+    field: 'newPriceStartTime',
+    headerName: '新单价执行时间',
+  },
+  {
+    field: 'priceBase',
+    headerName: '价格依据',
+  },
+  {
+    field: 'saleYn',
+    headerName: '是否寄售',
+    valueFormatter: function (params: any) {
+      return `格式化：${params.value === 'Y' ? '是' : '否'}`;
+    },
+  },
+  {
+    field: 'authorizeStartTime',
+    headerName: '授权有效期起',
+  },
+  {
+    field: 'authorizeEndTime',
+    headerName: '授权有效期止',
+  },
+  {
+    field: 'accountPeriod',
+    headerName: '在用付款账期（月）',
+  },
+  {
+    field: 'newAccountPeriod',
+    headerName: '新付款账期',
+  },
+  {
+    field: 'newAccountPeriodStartTime',
+    headerName: '新付款账期执行时间',
+  },
+  {
+    field: 'accountYn',
+    headerName: '耗材是否可以结账',
+  },
+  {
+    field: 'materialManufactureType',
+    headerName: '耗材类别',
+  },
+  {
+    field: 'materialChoiceType',
+    headerName: '耗材类型',
+  },
+  {
+    field: 'shippingManagementName',
+    headerName: '发运管理',
+    width: '160',
+  },
+  {
+    field: 'storeCondition',
+    headerName: '存储条件',
+  },
+  {
+    field: 'purchaseStyle',
+    headerName: '采购方式',
+  },
+  {
+    field: 'volumeBasedPurchaseYnName',
+    headerName: '是否带量采购',
+  },
+  {
+    field: 'tendersStyle',
+    headerName: '招标方式',
+  },
+  {
+    field: 'contractNo',
+    headerName: '合同编号',
+  },
+  {
+    field: 'position',
+    headerName: '合同存放位置',
+  },
+  {
+    field: 'materialStartTime',
+    headerName: '启用时间',
+  },
+  {
+    field: 'materialStopTime',
+    headerName: '停用时间',
+  },
+  {
+    field: 'fromMaterialStatus',
+    headerName: '耗材状态',
+  },
+  {
+    field: 'materialStatus',
+    headerName: '清单状态',
+  },
+  {
+    field: 'expiredYn',
+    headerName: '是否已过期',
+  },
+  {
+    field: 'updateUserName',
+    headerName: '修改人',
+  },
+  {
+    field: 'updateTime2',
+    headerName: '修改时间',
+  },
+  {
+    field: 'remark',
+    headerName: '备注',
+  },
+  {
+    field: 'auditRemark',
+    headerName: '准入审核备注',
+  },
+  {
+    field: 'action',
+    headerName: '操作',
+    pinned: 'right',
+    editable: false,
+  },
+];
--- src/views/component/grid/index.vue
+++ src/views/component/grid/index.vue
@@ -1,41 +1,131 @@
 <template>
-  <ag-grid-vue
-    style="height: 100%"
-    class="ag-theme-quartz"
-    :rowData="rowData"
-    :columnDefs="colDefs"
-/></template>
+  <a-card
+    title="耗材查询"
+    :bordered="false"
+    size="small"
+    class="shadow-sm rounded-16px w-full h-full"
+  >
+    <template #extra>
+      <a-switch v-model="formLayout" checked-value="right" unchecked-value="top" />
+    </template>
+    <a-layout style="height: 70vh">
+      <a-layout-sider v-if="formLayout === 'right'" :resize-directions="['right']">
+        <a-card :bordered="false" size="small" class="shadow-sm rounded-16px w-full">
+          <table-form layout="vertical" />
+        </a-card>
+      </a-layout-sider>
+      <a-layout :style="{ height: formLayout === 'top' ? '50vh' : '70vh' }">
+        <a-layout-header v-if="formLayout === 'top'">
+          <a-resize-box
+            :directions="['bottom']"
+            :style="{
+              height: '20vh',
+              textAlign: 'center',
+            }"
+          >
+            <a-card :bordered="false" size="small" class="shadow-sm rounded-16px w-full">
+              <table-form layout="inline" />
+            </a-card>
+          </a-resize-box>
+        </a-layout-header>
+        <a-layout-content>
+          <span class="mb-2 ml-2">已选择{{ total?.count }}条数据</span>
+          <ag-grid-vue
+            style="height: 95%"
+            class="ag-theme-quartz"
+            :rowData="rowData"
+            :columnDefs="colDefs"
+            :defaultColDef="defaultColDef"
+            :dataTypeDefinitions="dataTypeDefinitions"
+
+            rowSelection="multiple"
+            :suppressRowClickSelection="true"
+            :isRowSelectable="isRowSelectable"
+            @selectionChanged="onSelectionChanged"
+
+            :pagination="true"
+            :paginationAutoPageSize="true"
+            :paginateChildRows="true"
+          />
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-card>
+</template>
 
 <script>
-  import { ref } from 'vue';
+  import { ref, onBeforeMount } from 'vue';
   import 'ag-grid-community/styles/ag-grid.css'; // Core CSS
   import 'ag-grid-community/styles/ag-theme-quartz.css'; // Theme
   import { AgGridVue } from 'ag-grid-vue3'; // Vue Grid Logic
+
+  import { columns } from './columns';
+  import { tableData } from './data';
+
+  import tableForm from '../table/form.vue';
 
   export default {
     name: 'App',
     components: {
       AgGridVue, // Add AG Grid Vue3 component
+      tableForm,
     },
     setup() {
       // Row Data: The data to be displayed.
-      const rowData = ref([
-        { make: 'Tesla', model: 'Model Y', price: 64950, electric: true },
-        { make: 'Ford', model: 'F-Series', price: 33850, electric: false },
-        { make: 'Toyota', model: 'Corolla', price: 29600, electric: false },
-      ]);
+      const rowData = ref(tableData.data.rows.map((it,idx) => ({ ...it, isCheck: false, idx: idx+1 })));
 
       // Column Definitions: Defines & controls grid columns.
-      const colDefs = ref([
-        { field: 'make' },
-        { field: 'model' },
-        { field: 'price' },
-        { field: 'electric' },
-      ]);
+      const colDefs = ref(columns);
+
+      const defaultColDef = ref({
+        flex: 1,
+        minWidth: 140,
+        filter: true,
+        floatingFilter: false,
+        editable: true,
+      });
+
+      const dataTypeDefinitions = ref(null);
+
+    const isRowSelectable = ref(null);
+      onBeforeMount(() => {
+        dataTypeDefinitions.value = {
+          object: {
+            baseDataType: 'object',
+            extendsDataType: 'object',
+            valueParser: (params) => ({ name: params.newValue }),
+            valueFormatter: (params) => (params.value == null ? '' : params.value.name),
+          },
+        };
+
+        isRowSelectable.value = (params) => {
+        return  true;
+      };
+      });
+
+      // 表单布局
+      const formLayout = ref('right');
+      // 合计信息
+      const total = ref({ count: 0 });
+
+      const onSelectionChanged = event => {
+
+        const rows = event.api.getSelectedRows()
+        total.value.count = rows.length
+
+        
+      }
+
 
       return {
         rowData,
         colDefs,
+        defaultColDef,
+        dataTypeDefinitions,
+        formLayout,
+        total,
+        isRowSelectable,
+        onSelectionChanged
       };
     },
   };
